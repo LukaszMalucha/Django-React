e9b92ac4ded4c5ee766c9fb9f78f132638da3aa8
Lukasz Malucha <lucasmalucha@gmail.com>
2019-06-01 08:31:51 +0100

    init

---
+++ readme.md
@@ -0,0 +1,6 @@
+## Django Webpack
+
+
+npm init
+npm install -D @babel/core @babel/preset-env @babel/preset-react babel-loader css-loader node-sass webpack webpack-cli
+npm run webpack
---
+++ static/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render (
+    <div>
+        <h1> JSX </h1>
+    </div>,
+    document.getElementById('root')
+
+)
