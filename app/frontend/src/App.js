9615d3addd9532e7376db657f47be82e37e252c6
Lukasz Malucha <lucasmalucha@gmail.com>
2025-11-04 16:13:47 +0000

    components

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,8 +1,17 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div>
-      My App
+        <Header/>
+            <main class="py-3">
+                <Container>
+                <h1> Welcome </h1>
+                </Container>
+            </main>
+        <Footer/>
     </div>
   );
 }
---
+++ app/frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Row, Col } from "react-bootstrap"
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                Footer
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
